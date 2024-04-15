2d80e95b91c308bf6cf6d91e2c5e6bc677f679d4
Алексей <alexio.daily@gmail.com>
2024-04-15 19:10:03 +0300

    all&
    Я не до конца понимаю что делаю

---
+++ 4-navigator/navigator.js
@@ -0,0 +1,15 @@
+console.log('Домашнее задание блока 4')
+console.log('========================')
+
+let addressLat = 1;
+let addressLong = 5;
+
+let positionLat = 2;
+let positionLong = -4;
+
+let distance = Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2)
+
+console.log(`Считаем расстояние между [${addressLat}:${addressLong}] и [${positionLat}:${positionLong}]
+Расстояние составляет ${distance}`);
+
+console.log('========================')
